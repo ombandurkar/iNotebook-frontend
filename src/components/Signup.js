df333e21229de493b1e7d37ebe147e21d6fdeb27
Om Bandurkar <89293380+ombandurkar@users.noreply.github.com>
2023-08-03 10:56:25 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -18,7 +18,7 @@ const Signup = (props) => {
         // eslint-disable-next-line
         const { name, email, password } = credentials;  //destructuring kar rahe hai
 
-        const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+        const response = await fetch(`https://inotebook-backend-e4m1.onrender.com/api/auth/createuser`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
